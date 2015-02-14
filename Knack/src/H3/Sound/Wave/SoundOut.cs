0f7f1ca53ef32884444114e57c4a0b880513c8f4
Riccardo Gerosa <r.gerosa@gmail.com>
2015-02-14 23:42:43 +0100

    NAudio output WIP (now only playing a sine wave)

--- Knack/src/H3/Sound/Wave/SoundOut.cs
+++ Knack/src/H3/Sound/Wave/SoundOut.cs
@@ -23,7 +23,7 @@ using System.IO;
 using System.Threading;
 using System.Windows.Forms;
 #if PLATFORM_WIN32  
-	using Microsoft.DirectX.DirectSound;
+    using NAudio.Wave;
 #endif
 
 using H3.Utils;
@@ -31,9 +31,9 @@ using H3.Utils;
 namespace H3.Sound.Wave
 {
 	/// <summary>
-	/// A managed DirectX 9 sound player
+	/// A NAudio sound player
 	/// </summary>
-	public class SoundOut : IDisposable
+	public class SoundOut : WaveProvider32, IDisposable
 	{
 		int samplesPerSecond = 44100;
 		int channels = 2;
@@ -41,16 +41,11 @@ namespace H3.Sound.Wave
 		int bufferBlockBytes = 3200;
 		int bufferBytes = 3200 * 8;  // This must be a multiple of bufferBlockBytes !
 		int sleepTime = 5;
-		Device soundDevice;
-		SecondaryBuffer soundBuffer;
-		BufferDescription soundBufferDescription;
-		WaveFormat soundWaveFormat = new WaveFormat();
-		System.Timers.Timer soundTimer;
+        WaveOut waveOut;
 		FileStream fs;
 	    BinaryWriter w;
 		bool outputToFile = false;
-		int completedPos = 0;
-		StreamingType streamingType = StreamingType.Thread;
+		//int completedPos = 0;
 		short[] blockData;
 		float[] floatLeftChannel;
 		float[] floatRightChannel;
@@ -74,13 +69,15 @@ namespace H3.Sound.Wave
 				*bufferBlockBytes;
 			sleepTime = Settings.Instance.GetInt(
 				"/Settings/Output/Sound/DirectSound/ThreadSleepTime");
+
+            waveOut = new WaveOut();
+            waveOut.Init(this);
+            waveOut.Play();
 			
-			soundDevice = new Device();
-			soundDevice.SetCooperativeLevel(new Form(), CooperativeLevel.Normal);  
 			blockData = new short[bufferBlockBytes];
 			floatLeftChannel = new float[bufferBlockBytes/2];
 			floatRightChannel = new float[bufferBlockBytes/2];
-			
+			/*
 			soundWaveFormat.SamplesPerSecond = samplesPerSecond;
 			soundWaveFormat.Channels = (short) channels;
 			soundWaveFormat.BitsPerSample = (short) bitsPerSample;
@@ -95,10 +92,10 @@ namespace H3.Sound.Wave
 			soundBufferDescription.CanGetCurrentPosition = true;
 			soundBufferDescription.ControlVolume = true;  
 			soundBufferDescription.Format = soundWaveFormat;
-			
-			soundBuffer = new SecondaryBuffer(soundBufferDescription,soundDevice);
-			bufferBytes = soundBuffer.Caps.BufferBytes;
-			
+			*/
+			//soundBuffer = new SecondaryBuffer(soundBufferDescription,soundDevice);
+			//bufferBytes = soundBuffer.Caps.BufferBytes;
+			/*
 			if (streamingType == StreamingType.Timer) {
 				soundTimer = new System.Timers.Timer(sleepTime);
 				soundTimer.Enabled = false;
@@ -107,8 +104,9 @@ namespace H3.Sound.Wave
 			
 			blockData.Initialize();
 			for (int i = 0; i<bufferBytes/bufferBlockBytes; i++)  {
-				soundBuffer.Write(blockData.Length*i, blockData, LockFlag.EntireBuffer);  
+				//soundBuffer.Write(blockData.Length*i, blockData, LockFlag.EntireBuffer);  
 			}
+             * */
 			
 		}
 		
@@ -119,8 +117,6 @@ namespace H3.Sound.Wave
 		
 		public void Dispose()
    		{
-			
-			if (streamingType == StreamingType.Timer) soundTimer.Dispose();
      	 	Dispose(true);
      		GC.SuppressFinalize(this);
   		}
@@ -134,66 +130,18 @@ namespace H3.Sound.Wave
          		{
             		// Dispose managed resources.
 					Stop();
+                    waveOut.Dispose();
+                    waveOut = null;
          		}
          		// Release unmanaged resources. 
       		}
       		disposed = true;         
    		}
 		
-		private void SoundTimerElapsed(object sender, System.Timers.ElapsedEventArgs e) 
-		{
-			int playPos,writePos;
-			bool tryagain;
-			LockFlag lockFlag = LockFlag.None;
-
-			if (blockData == null) return;
-								
-			do {
-				playPos = soundBuffer.PlayPosition;
-				writePos = soundBuffer.WritePosition;
-				//System.Console.WriteLine("current: "+playPos+" write: "+writePos+" diff: "+(writePos-playPos)+" completed: "+ completedPos);
-				
-				tryagain = false;
-				if (playPos > completedPos) {
-					if ((playPos-completedPos) >= bufferBlockBytes*2) {
-						Render(blockData);
-						soundBuffer.Write(completedPos,blockData,lockFlag);
-						completedPos += bufferBlockBytes*2;
-						tryagain = true;
-					}
-				}
-				else {
-					if ((bufferBytes-completedPos) >= bufferBlockBytes*2) {
-						Render(blockData);
-						soundBuffer.Write(completedPos,blockData,lockFlag);
-						completedPos += bufferBlockBytes*2;
-						tryagain = true;
-					}
-				}
-				if (completedPos >= bufferBytes) completedPos = 0;
-			} while (tryagain);
-		}
-		
 		public void Start() 
-		{	
-			soundBuffer.SetCurrentPosition(0);
-			soundBuffer.Play(0, BufferPlayFlags.Looping);
-			
-			if (streamingType == StreamingType.Timer) {
-				System.Threading.Thread.Sleep(1);
-				soundTimer.Enabled = true;
-			}
-			
-			if (streamingType == StreamingType.Thread) {
-				Thread soundThread = new Thread(new ThreadStart(SoundThread));  
-				soundThread.Name = "SoundThread";  
-				soundThread.Priority = ThreadPriority.Highest;  
-				soundThread.IsBackground = true;
-				soundThread.Start();  
-			}
-			
-			System.Console.WriteLine("SoundOut started");
-			
+		{
+            System.Console.WriteLine("SoundOut started");
+            waveOut.Play();
 			if (outputToFile) {
 				fs = new FileStream("knack.raw", FileMode.CreateNew);
 		        w = new BinaryWriter(fs);
@@ -202,12 +150,9 @@ namespace H3.Sound.Wave
 		
 		public void Stop() 
 		{
-			System.Console.WriteLine("SoundOut stopped");
-			if (streamingType == StreamingType.Timer) {
-				if (soundTimer!= null) soundTimer.Enabled = false;
-			}
-			if (soundBuffer != null) {
-				if (soundBuffer.Status.Playing) soundBuffer.Stop();
+            System.Console.WriteLine("SoundOut stopped");
+			if (waveOut != null) {
+                waveOut.Stop();
 			}
 			if (outputToFile) {
 				w.Close();
@@ -215,50 +160,59 @@ namespace H3.Sound.Wave
 			}
 		}
 		
-		private void SoundThread()  
-		{  
-			for (;;) {
-				System.Threading.Thread.Sleep(sleepTime);
-				SoundTimerElapsed(null,null);
-			}
-		}  
-		
 		/// <summary>
 		/// Override this method to output a sound
 		/// </summary>
 		public virtual void Render(float[] leftChannel,float[] rightChannel)
 		{ }
 		
-		private void Render(short[] interleavedChannel)
-			{
-				int channelSize = interleavedChannel.Length/2;
+		private void Render(short[] interleavedChannel) 
+        {
+			int channelSize = interleavedChannel.Length/2;
 				
-				float wave;
+			float wave;
 			
-				for (int i=0; i<channelSize; i++) {
-					floatLeftChannel[i] = 0.0f;
-				}
-				for (int i=0; i<channelSize; i++) {
-					floatRightChannel[i] = 0.0f;
-				}
+			for (int i=0; i<channelSize; i++) {
+				floatLeftChannel[i] = 0.0f;
+			}
+			for (int i=0; i<channelSize; i++) {
+				floatRightChannel[i] = 0.0f;
+			}
 				
-				this.Render(floatLeftChannel,floatRightChannel);
+			this.Render(floatLeftChannel,floatRightChannel);
 			
-				for (int i=0; i<channelSize; i++) {
-					wave = floatLeftChannel[i];
-					if (wave>1.0f) wave = 1.0f; else if (wave<-1.0f) wave = -1.0f;
-					interleavedChannel[i*2] = (short) Math.Round(wave * 32767.0f);
-					wave = floatRightChannel[i];
-					if (wave>1.0f) wave = 1.0f; else if (wave<-1.0f) wave = -1.0f;
-					interleavedChannel[i*2+1] = (short) Math.Round(wave * 32767.0f);
-				}
-				if (outputToFile) {
-					if (w!=null)
-					for (int i=0; i<interleavedChannel.Length; i++) {
-						w.Write(interleavedChannel[i]);
-					}
+			for (int i=0; i<channelSize; i++) {
+				wave = floatLeftChannel[i];
+				if (wave>1.0f) wave = 1.0f; else if (wave<-1.0f) wave = -1.0f;
+				interleavedChannel[i*2] = (short) Math.Round(wave * 32767.0f);
+				wave = floatRightChannel[i];
+				if (wave>1.0f) wave = 1.0f; else if (wave<-1.0f) wave = -1.0f;
+				interleavedChannel[i*2+1] = (short) Math.Round(wave * 32767.0f);
+			}
+			if (outputToFile) {
+				if (w!=null)
+				for (int i=0; i<interleavedChannel.Length; i++) {
+					w.Write(interleavedChannel[i]);
 				}
 			}
+		}
+
+        int sample;
+        float Frequency = 1000;
+        float Amplitude = 0.25f;
+
+        public override int Read(float[] buffer, int offset, int sampleCount)
+        {
+            int sampleRate = WaveFormat.SampleRate;
+            for (int n = 0; n < sampleCount; n++)
+            {
+                buffer[n + offset] = (float)(Amplitude * Math.Sin((2 * Math.PI * sample * Frequency) / sampleRate));
+                sample++;
+                if (sample >= sampleRate) sample = 0;
+            }
+            return sampleCount;
+        }
 		
 	}
+   
 }
